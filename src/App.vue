5eb5b52eec10ad075d9df898f71d8f124e48f2cc
wangyuqiang <994346021@qq.com>
2024-06-20 08:41:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default{
+  data(){
+    return {
+      userName:'Tom',
+      salary:12000
+    }
+  },
+  methods:{
+    addSalary(){
+      this.salary += 1000
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+姓名：<input v-model="userName"/> {{ userName }}<br />
+薪水：<input type="number" v-model="salary"/> {{ salary }}<br />
+<button @click="addSalary()">提交</button>
+    </div>
+</template>
+
+<style>
+
+</style>
